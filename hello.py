5103b5b9e8a50d09a7581097e012a6f259a9b5fe
Sopuruchi <118601888+cs-uche@users.noreply.github.com>
2022-12-02 14:02:40 +0000

    hello.py using pdb

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 def add_number(x,y):
+    import pdb; pdb.set_trace()
     '''This is a basic function that sums 2 digits'''
-    # debuggins with print
+    # debugging with pdb(python debugger)
+    '''
+    We use pdb as a normal debugger we
+    n - takes us to the next line of code
+    x - shows us what is in the variable x
+    y - likewise displays content of variable y
+    result - displays contents of var result
+    exit() - exits the debugger
+
+    We can also perform expressions in the debugger as we would in any python terminal
+    '''
     print(f"This is x:{x} of type {type(x)} and this is y:{y} of type {type(y)}")
 
     try:
@@ -12,16 +23,22 @@ def add_number(x,y):
     print(f"This is the reuslt:{result}")
     return result
 
-print(add_number("1",2))
-
+print(add_number("one",2))
 
 
 # def add_number(x,y):
 #     '''This is a basic function that sums 2 digits'''
-#     # debuggins with print
-#     print(f"This is x:{x} and this is y:{y}")
-#     result = x+y
+#     # debugging with print
+#     print(f"This is x:{x} of type {type(x)} and this is y:{y} of type {type(y)}")
+
+#     try:
+#         result = x+y
+#     except TypeError as exception:
+#         print(f"The wrong type was passed in {exception}")
+#         result = int(x)+int(y)
+
 #     print(f"This is the reuslt:{result}")
 #     return result
 
-# print(add_number(1,2))
+# print(add_number("1",2))
+
